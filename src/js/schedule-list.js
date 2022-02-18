106fbd9fd87bfc16543986eb585af5e80be63db7
HanSeongJae <fhqhxm8899@gmail.com>
2022-02-19 01:47:34 +0900

    <FEAT> result schdule의 항목 클릭시 삭제

--- src/js/schedule-list.js
+++ src/js/schedule-list.js
@@ -1,20 +1,44 @@
 const listElement = document.querySelectorAll(".table-component__list__element");
+let scheduleElement = document.querySelectorAll(".result-schedule__element");
 
+let length = scheduleElement.length;
 
 const handleListClick = (event) => {
+    // event.preventdefault();
+    event.preventDefault();
     console.log("hello");
     const test = document.createElement("div");
     const inText = event.path[0].innerText;
     console.log(event);
-    test.innerHTML =`<a href = "#" class="table-component__list__element">
-    <li>
+    
+
+    test.innerHTML =`<li class="table-component__list__element result-schedule__element">
+    <a>
         <span>${inText}</span>
-        <!--todo: icon chevron-right-->
-    </li>
-</a>`;
+        <i class="fa-solid fa-chevron-right fa-lg"></i>
+    </a>
+</li>`;
     document.querySelector(".result-schedule").appendChild(test);
+
+    scheduleElement = document.querySelectorAll(".result-schedule__element");
+    length = scheduleElement.length;
+    console.log(scheduleElement);
+    
+    for(let i = 0; i<length; i++){
+        scheduleElement[i].addEventListener("click",handleS);
+    }
 }
 
-for(let i = 0; i<4; i++){
+const handleS = (event) => {
+    event.preventDefault();
+    const chosenItem = event.currentTarget.parentNode
+    console.log(chosenItem.parentNode.removeChild(chosenItem));
+}
+
+
+
+
+for(let i = 0; i<listElement.length; i++){
     listElement[i].addEventListener("click",handleListClick);
 }
+
