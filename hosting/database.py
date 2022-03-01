0b162e5fe26b8a07d206e3dc624d18566ad3ecd4
chldbswodlqslek <janek4@cau.kr>
2022-03-01 21:39:30 +0900

    FEAT: 정적 데이터 반환 api

--- hosting/database.py
+++ hosting/database.py
@@ -1,13 +1,21 @@
 from django.db import connection
+import pymysql.cursors
+import pandas as pd
 
 def GetQuerySetFromDataBases(request):
     try:
+        connection = pymysql.connect(host='localhost', port=3306, user='root',
+                                     password='freefree104460!', db='project',
+                                     charset='utf8',autocommit=True,
+                                     cursorclass=pymysql.cursors.DictCursor)
         cursor = connection.cursor()
-        result = cursor.execute(request)
+        cursor.execute(request)
         result = cursor.fetchall()
         connection.commit()
         print(result)
         connection.close()
+        result = pd.DataFrame(result)
+        print(result)
 
     except:
         connection.rollback()
