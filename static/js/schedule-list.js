9aad847c697d4e1e5b0d8a95111a93c6a5fe0239
HanSeongJae <fhqhxm8899@gmail.com>
2022-03-07 01:08:42 +0900

    <FEAT>: static 파일 추가

--- static/js/schedule-list.js
+++ static/js/schedule-list.js
@@ -1,44 +1,43 @@
-const listElement = document.querySelectorAll(".table-component__list__element");
+const listElement = document.querySelectorAll(
+  ".table-component__list__element"
+);
 let scheduleElement = document.querySelectorAll(".result-schedule__element");
 
 let length = scheduleElement.length;
 
-const handleListClick = (event) => {
-    // event.preventdefault();
-    event.preventDefault();
-    console.log("hello");
-    const test = document.createElement("div");
-    const inText = event.path[0].innerText;
-    console.log(event);
-    
+//api 요청
+// console.log(JSON.parse(localStorage.getItem("cast")));
 
-    test.innerHTML =`<li class="table-component__list__element result-schedule__element">
-    <a>
+const handleListClick = (event) => {
+  event.preventDefault();
+  const selectedElement = document.createElement("li");
+  selectedElement.classList.add("table-component__list__element");
+  selectedElement.classList.add("result-schedule__element");
+
+  const inText = event.path[0].innerText;
+  console.log(event);
+
+  selectedElement.innerHTML = `<a>
         <span>${inText}</span>
         <i class="fa-solid fa-chevron-right fa-lg"></i>
-    </a>
-</li>`;
-    document.querySelector(".result-schedule").appendChild(test);
+    </a>`;
+  document.querySelector(".result-schedule").appendChild(selectedElement);
 
-    scheduleElement = document.querySelectorAll(".result-schedule__element");
-    length = scheduleElement.length;
-    console.log(scheduleElement);
-    
-    for(let i = 0; i<length; i++){
-        scheduleElement[i].addEventListener("click",handleS);
-    }
-}
+  scheduleElement = document.querySelectorAll(".result-schedule__element");
+  length = scheduleElement.length;
+  console.log(scheduleElement);
+
+  for (let i = 0; i < length; i++) {
+    scheduleElement[i].addEventListener("click", handleS);
+  }
+};
 
 const handleS = (event) => {
-    event.preventDefault();
-    const chosenItem = event.currentTarget.parentNode
-    console.log(chosenItem.parentNode.removeChild(chosenItem));
+  event.preventDefault();
+  const chosenItem = event.currentTarget;
+  console.log(chosenItem.parentNode.removeChild(chosenItem));
+};
+
+for (let i = 0; i < listElement.length; i++) {
+  listElement[i].addEventListener("click", handleListClick);
 }
-
-
-
-
-for(let i = 0; i<listElement.length; i++){
-    listElement[i].addEventListener("click",handleListClick);
-}
-
--- static/js/setting-screen.js
+++ static/js/setting-screen.js
@@ -1,67 +1,142 @@
-
-
 //html 요소 선택자
-const listElement = document.querySelectorAll(".table-component__list__element");
+const listElement = document.querySelectorAll(
+  ".table-component__list__element"
+);
 const listTitle = document.querySelector(".table-component__header h4");
 const progressNum = document.querySelector("main>span");
 
-//필요한 데이터
-const optionList ={
-    title : {
-        department : "내 소속 학과는?",
-        admissionPath : "내 입과 경로는?",
-        admissionYear : "내 학번은?",
-        detailedMajor : "내 세부 전공은?",
-        track : "내 트랙은?"
-    },
-
-    department : ["공공인재학부","소프트웨어학부"],
-    admissionPath : ["신입학","전과","편입학"],
-    admissionYear : ["2022","2021","2020","2019","2018"],
-    detailedMajor : ["전공심화","복수전공 (주전공)", "복수전공 (부전공)","연계전공 이름"],
-    track : ["해당없음","행정학 트랙","정책학 트랙","엔터프라이즈 SW"],
-
-
-};
-const textList = ["department","admissionPath","admissionYear","detailedMajor","track"];
-
 //변수
 let currentProgress = 1;
-let whatYouGot = [];
+let userChoice = [];
 
+//필요한 데이터
 
+//필요한 데이터 -- 제목 리스트
+const title = {
+  department: "내 소속 학과는?",
+  admissionPath: "내 입과 경로는?",
+  admissionYear: "내 학번은?",
+  detailedMajor: "내 세부 전공은?",
+  track: "내 트랙은?",
+};
+
+//필요한 데이터 -- 선택지
+const options = {
+  department: ["공공인재학부", "소프트웨어학부"],
+  admissionPath: ["신입학", "전과", "편입학"],
+  admissionYear: ["2022", "2021", "2020", "2019", "2018"],
+  detailedMajor: [
+    "전공심화",
+    "복수전공 (주전공)",
+    "복수전공 (부전공)",
+    "연계전공 이름",
+  ],
+  track: ["해당없음", "행정학 트랙", "정책학 트랙", "엔터프라이즈 SW"],
+};
+
+//필요한 데이터 -- 더미데이터
+const selectedDepartment = {
+  공공인재학부: {
+    detailedMajor: ["공공인재1", "공공인재2"],
+    track: ["공공인재3", "공공인재4"],
+  },
+  소프트웨어학부: {
+    detailedMajor: ["소프트웨어1", "소프트웨어2"],
+    track: ["소프트웨어3", "소프트웨어4"],
+  },
+};
+
+//필요한 데이터 -- key 배열
+const keyLists = [
+  "department",
+  "admissionPath",
+  "admissionYear",
+  "detailedMajor",
+  "track",
+];
+
+//inner 함수
 const moveToSchedulePage = () => {
-    location.href="schedulling-page.html"
-}
+  location.href = "schedulling-page.html";
+};
 
-const handleListClick = (event) => {
-    currentProgress += 1;
-    if(currentProgress === 6){
-        moveToSchedulePage();
+const optionWriter = (optionText) => {
+  const optionElment = document.createElement("li");
+  optionElment.classList.add("table-component__list__element");
+  optionElment.innerHTML = `<a>
+      <span>${optionText}</span>
+      <i class="fa-solid fa-chevron-right fa-lg"></i>
+  </a>`;
+
+  document.querySelector(".table-component__list").appendChild(optionElment);
+};
+
+const optionCleaner = () => {
+  let optionTable = document.querySelector(".table-component__list");
+  optionTable.innerHTML = "";
+};
+
+const optionsReSetter = (selected) => {
+  options.detailedMajor = selectedDepartment[selected].detailedMajor;
+  options.track = selectedDepartment[selected].track;
+};
+
+//핸들러
+const handleOptionButtonClick = (event) => {
+  currentProgress += 1;
+  if (currentProgress <= keyLists.length) {
+    CURRENTINDEX = keyLists[currentProgress - 1];
+
+    if (CURRENTINDEX === "admissionPath") {
+      optionsReSetter(event.currentTarget.innerText);
     }
-
 
     console.log(event);
-    whatYouGot.push(event.path[0].innerText);
-    console.log(whatYouGot);
-    listTitle.innerText = optionList.title[textList[currentProgress-1]];
-    let number = 0;
-    for(number =1; number < optionList[textList[currentProgress-1]].length; number++){
-        const mainChoice = document.querySelector(`.table-component__list__element:nth-child(${number}) span`);
-        mainChoice.innerHTML = `${optionList[textList[currentProgress-1]][number]}`
+    userChoice.push(event.currentTarget.innerText);
+    console.log(userChoice);
+    console.log(CURRENTINDEX);
+
+    //핸들러__제목 띄우기
+    listTitle.innerText = title[CURRENTINDEX];
+
+    //핸들러__선택지 띄우기
+    optionCleaner();
+    for (
+      optionNumber = 0;
+      optionNumber < options[CURRENTINDEX].length;
+      optionNumber++
+    ) {
+      optionWriter(options[CURRENTINDEX][optionNumber]);
     }
-    progressNum.innerText = `${currentProgress}/5`
-    
-    
 
+    optionElement = document.querySelectorAll(
+      ".table-component__list__element"
+    );
+    length = optionElement.length;
+    console.log(optionElement);
+
+    //핸들러__이벤트 핸들러 재추가
+    for (let i = 0; i < length; i++) {
+      optionElement[i].addEventListener("click", handleOptionButtonClick);
+    }
+
+    //핸들러__하단 현재 페이지 설정
+    progressNum.innerText = `${currentProgress}/5`;
+  } else {
+    //api 요청
+    const cast = {
+      department: userChoice[0],
+      major: userChoice[1],
+      track: userChoice[2],
+      entry_year: userChoice[3],
+    };
+    console.log(cast);
+    localStorage.setItem("cast", JSON.stringify(cast));
+    moveToSchedulePage();
+  }
+};
+
+//이벤트 핸들러 추가
+for (let i = 0; i < listElement.length; i++) {
+  listElement[i].addEventListener("click", handleOptionButtonClick);
 }
-
-
-//이벤트 핸들러
-for(let i = 0; i<4; i++){
-    listElement[i].addEventListener("click",handleListClick);
-}
-
-
-
-        
